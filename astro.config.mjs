138cfecd852ff1c049eb99c5cd8373b42e0d8316
PaulHaze <dev@artemist.com.au>
2024-06-17 19:19:34 +1000

    add sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
+	site: 'https://astrofied.pages.dev/',
 	integrations: [
+		sitemap(),
 		tailwind({
 			applyBaseStyles: false,
 		}),
