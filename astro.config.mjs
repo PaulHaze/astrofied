1012a7049586185ff5eebebe972841dd72f97643
PaulHaze <dev@artemist.com.au>
2024-06-17 15:50:32 +1000

    add cloudfare adaptor for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import icon from 'astro-icon';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
-		icon(),
-	],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), icon()],
+  output: "server",
+  adapter: cloudflare()
 });
