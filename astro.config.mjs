f08b4e5885928d9973dd5fe80a1f81630b35987b
PaulHaze <dev@artemist.com.au>
2024-06-17 15:09:30 +1000

    update astro config to use github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://paulhaze.github.io',
+	base: 'astrofied',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
