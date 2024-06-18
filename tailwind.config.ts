6b63cdd4c2e64b793389655784e1c71832b6849a
PaulHaze <dev@artemist.com.au>
2024-06-19 08:58:44 +1000

    Header component done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -140,7 +140,7 @@ export default {
 		}),
 	],
 	daisyui: {
-		themes: ['light', 'dark'],
+		themes: true,
 		// themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: 'dark', // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
