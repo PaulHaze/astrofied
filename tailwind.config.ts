4bed820e361225f9a6b41c8c1fb01cb48488542f
PaulHaze <dev@artemist.com.au>
2024-06-18 13:18:51 +1000

    add dark light theme to daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -138,7 +138,8 @@ export default {
 		}),
 	],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: ['light', 'dark'],
+		// themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: 'dark', // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	},
