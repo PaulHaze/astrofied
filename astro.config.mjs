c89f43bf2096d9c73684e82f928c01cfb15d6016
PaulHaze <dev@artemist.com.au>
2024-06-18 12:58:48 +1000

    comment out cloudflare adaptor import

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 	site: 'https://astrofied.pages.dev/',
