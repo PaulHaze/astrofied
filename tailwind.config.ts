5f95d4e37362a6412299071d856ed1e2e2fb5a93
PaulHaze <dev@artemist.com.au>
2024-06-25 13:46:57 +1000

    add detailed theme examples to TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -146,6 +146,46 @@ export default {
 				light: {
 					...require('daisyui/src/theming/themes')['light'],
 					// add individual colours here to customize theme
+					/* 
+					*EXAMPLE - ESSENTIAL
+					"primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+
+					*EXAMPLE - OPTIONAL
+					* These are set automatically based on the colours chosen above
+					* But they can be overriden
+					"primary-content": "#d0e3ff", - Colour of text in 'Primary'
+					"secondary-content": "#0a0900", - Colour of text in 'Secondary'
+					"accent-content": "#00050d",- Colour of text in 'Accent'
+					"neutral-content": "#d2cdd2", - Colour of text in 'Neutral
+					"base-200": "#dbdbdb", - Slightly darker shade of base-100
+					"base-300": "#bbbbbb", - Even darker shade of base-100
+					"base-content": "#161616", - Contrasting colour for text on base-100
+					
+					* Additional colours to be added if needed
+					"info": "#00b1ff",
+					"info-content": "#000c16",
+					"success": "#85ee00",
+					"success-content": "#061400",
+					"warning": "#c28700",
+					"warning-content": "#0e0600",
+					"error": "#ff768e",
+					"error-content": "#160507",
+
+
+					* Additional CSS variables used
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs */
 				},
 			},
 			{
